ef09efacd833bd893a0c77ebafdb78e6edead21d
gihyun <gihyunkwak@gmail.com>
2016-10-16 17:37:14 +0900

    complete cal funtion change layout

--- app/src/main/java/com/gkwak/earningscalculator/MainActivity.java
+++ app/src/main/java/com/gkwak/earningscalculator/MainActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DecimalFormat;
@@ -21,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
             + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
     private static final String TAG = "MAIN_ACTIVITY";
 
-    private EditText monthly_rent_edit, rent_deposit_edit, market_price_edit;
+    private EditText monthly_rent_edit, rent_deposit_edit, buy_total_price_edit,
+            loan_price_edit, loan_rate_edit;
+    private TextView market_price_edit, rent_rate_edit, yearly_rent_revenu_edit,
+            yearly_interest_edit, real_investment_edit, yearly_pure_revenu_edit,
+            monthly_pure_revenu_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,30 +38,44 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder()
                 .setRequestAgent("android_studio:ad_template").build();
         adView.loadAd(adRequest);
-//        AdView mAdView = (AdView) findViewById(R.id.adView);
-//        AdRequest adRequest = new AdRequest.Builder().build();
-//        assert mAdView != null;
-//        mAdView.loadAd(adRequest);
 
-        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
-
-
+        // 1st Layout
         rent_deposit_edit = (EditText) findViewById(R.id.rent_deposit_edit);
         monthly_rent_edit = (EditText) findViewById(R.id.monthly_rent_edit);
-        market_price_edit = (EditText) findViewById(R.id.market_price_edit);
+        market_price_edit = (TextView) findViewById(R.id.market_price_edit);
+
+        // 2st Layout
+        buy_total_price_edit = (EditText) findViewById(R.id.buy_total_price_edit);
+        loan_price_edit = (EditText) findViewById(R.id.loan_price_edit);
+        loan_rate_edit = (EditText) findViewById(R.id.loan_rate_edit);
+
+        // 3st Layout
+        rent_rate_edit = (TextView) findViewById(R.id.rent_rate_edit);
+        yearly_rent_revenu_edit = (TextView) findViewById(R.id.yearly_rent_revenu_edit);
+        yearly_interest_edit = (TextView) findViewById(R.id.yearly_interest_edit);
+        real_investment_edit = (TextView) findViewById(R.id.real_investment_edit);
+        yearly_pure_revenu_edit = (TextView) findViewById(R.id.yearly_pure_revenu_edit);
+        monthly_pure_revenu_edit = (TextView) findViewById(R.id.monthly_pure_revenu_edit);
 
         // 세자리로 끊어서 쉼표 보여주고, 소숫점 셋째짜리까지 보여준다.
         final DecimalFormat df = new DecimalFormat("###,###.####");
 // 값 셋팅시, StackOverFlow를 막기 위해서, 바뀐 변수를 저장해준다.
-        final String[] result = {""};
+        // 1st Layout
+        final String[] rent_deposit_result = {""};
+        final String[] monthly_rent_result = {""};
 
+        // 2st Layout
+        final String[] buy_total_price_result = {""};
+        final String[] loan_price_result = {""};
+        final String[] loan_rate_result = {""};
+
+        // 1st Layout
         rent_deposit_edit.addTextChangedListener(new TextWatcher(){
             public void afterTextChanged(Editable s) {}
             public void beforeTextChanged(CharSequence s, int start, int count, int after){}
             public void onTextChanged(CharSequence s, int start, int before, int count){
 
-                if(!s.toString().equals(result[0])){     // StackOverflow를 막기위해,
+                if(!s.toString().equals(rent_deposit_result[0])){     // StackOverflow를 막기위해,
 
                     if (s.length() == 0) rent_deposit_edit.setText(0+"");
                     Long rentDeposit = Long.parseLong(rent_deposit_edit.getText().toString().replaceAll(",", ""));
@@ -64,9 +83,14 @@ public class MainActivity extends AppCompatActivity {
                     Long calResult = rentDeposit + (monthlyRent * 200);
                     market_price_edit.setText(df.format(calResult)+"");
 
-                    result[0] = df.format(Long.parseLong(s.toString().replaceAll(",", "")));   // 에딧텍스트의 값을 변환하여, result에 저장.
-                    rent_deposit_edit.setText(result[0]);    // 결과 텍스트 셋팅.
-                    rent_deposit_edit.setSelection(result[0].length());     // 커서를 제일 끝으로 보냄.
+                    Long buyTotalPrice = Long.parseLong(buy_total_price_edit.getText().toString().replaceAll(",", ""));
+                    Long loanPrice = Long.parseLong(loan_price_edit.getText().toString().replaceAll(",", ""));
+                    Long realInvestmentCalResult = buyTotalPrice - rentDeposit - loanPrice;
+                    real_investment_edit.setText(df.format(realInvestmentCalResult)); // 실투자비
+
+                    rent_deposit_result[0] = df.format(Long.parseLong(s.toString().replaceAll(",", "")));   // 에딧텍스트의 값을 변환하여, result에 저장.
+                    rent_deposit_edit.setText(rent_deposit_result[0]);    // 결과 텍스트 셋팅.
+                    rent_deposit_edit.setSelection(rent_deposit_result[0].length());     // 커서를 제일 끝으로 보냄.
                 }
             }
         });
@@ -75,24 +99,151 @@ public class MainActivity extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after){}
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                if (!s.toString().equals(result[0])) {
+                if (!s.toString().equals(monthly_rent_result[0])) {
                     Log.i(TAG, " a : " + s.toString());
                     if (s.length() == 0) monthly_rent_edit.setText(0 + "");
                     Long rentDeposit = Long.parseLong(rent_deposit_edit.getText().toString().replaceAll(",", ""));
                     Long monthlyRent = Long.parseLong(monthly_rent_edit.getText().toString().replaceAll(",", ""));
-                    Long calResult = rentDeposit + (monthlyRent * 200);
-                    market_price_edit.setText(df.format(calResult) + "");
 
-                    result[0] = df.format(Long.parseLong(s.toString().replaceAll(",", "")));   // 에딧텍스트의 값을 변환하여, result에 저장.
-                    monthly_rent_edit.setText(result[0]);    // 결과 텍스트 셋팅.
-                    monthly_rent_edit.setSelection(result[0].length());     // 커서를 제일 끝으로 보냄.
+                    Long calResult = rentDeposit + (monthlyRent * 200);
+                    market_price_edit.setText(df.format(calResult) + ""); // 적정시세
+
+                    Long yearlyRentRevenuCalResult = monthlyRent * 12;
+                    yearly_rent_revenu_edit.setText(df.format(yearlyRentRevenuCalResult) + ""); // 연간 임대료 수입
+
+                    monthly_rent_result[0] = df.format(Long.parseLong(s.toString().replaceAll(",", "")));   // 에딧텍스트의 값을 변환하여, result에 저장.
+                    monthly_rent_edit.setText(monthly_rent_result[0]);    // 결과 텍스트 셋팅.
+                    monthly_rent_edit.setSelection(monthly_rent_result[0].length());     // 커서를 제일 끝으로 보냄.
                 }
             }
         });
 
+        //2st Layout
+        buy_total_price_edit.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
+                if (!s.toString().equals(buy_total_price_result[0])) {
+                    Log.i(TAG, " a : " + s.toString());
+                    if (s.length() == 0) buy_total_price_edit.setText(0 + "");
 
+                    Long rentDeposit = Long.parseLong(rent_deposit_edit.getText().toString().replaceAll(",", ""));
+                    Long buyTotalPrice = Long.parseLong(buy_total_price_edit.getText().toString().replaceAll(",", ""));
+                    Long loanPrice = Long.parseLong(loan_price_edit.getText().toString().replaceAll(",", ""));
+                    Long realInvestmentCalResult = buyTotalPrice - rentDeposit - loanPrice;
+                    real_investment_edit.setText(df.format(realInvestmentCalResult)); // 실투자비
 
+                    buy_total_price_result[0] = df.format(Long.parseLong(s.toString().replaceAll(",", "")));   // 에딧텍스트의 값을 변환하여, result에 저장.
+                    buy_total_price_edit.setText(buy_total_price_result[0]);    // 결과 텍스트 셋팅.
+                    buy_total_price_edit.setSelection(buy_total_price_result[0].length());     // 커서를 제일 끝으로 보냄.
+                }
+            }
+        });
+        loan_price_edit.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (!s.toString().equals(loan_price_result[0])) {
+                    Log.i(TAG, " a : " + s.toString());
+                    if (s.length() == 0) {
+                        loan_price_edit.setText(0 + "");
+                        return;
+                    }
+                    Float loanPrice = Float.parseFloat(loan_price_edit.getText().toString().replaceAll(",", ""));
+                    Float loanRate = Float.parseFloat(loan_rate_edit.getText().toString().replaceAll(",", ""));
+                    Float yearlyRevenuCalResult = (loanPrice * loanRate) / 100;
+
+                    yearly_interest_edit.setText(df.format(Math.round(yearlyRevenuCalResult)));
+
+                    Long rentDeposit = Long.parseLong(rent_deposit_edit.getText().toString().replaceAll(",", ""));
+                    Long buyTotalPrice = Long.parseLong(buy_total_price_edit.getText().toString().replaceAll(",", ""));
+                    Long realInvestmentCalResult = buyTotalPrice - rentDeposit - Math.round(loanPrice);
+                    real_investment_edit.setText(df.format(realInvestmentCalResult)); // 실투자비
+
+                    loan_price_result[0] = df.format(Long.parseLong(s.toString().replaceAll(",", "")));   // 에딧텍스트의 값을 변환하여, result에 저장.
+                    loan_price_edit.setText(loan_price_result[0]);    // 결과 텍스트 셋팅.
+                    loan_price_edit.setSelection(loan_price_result[0].length());     // 커서를 제일 끝으로 보냄.
+                }
+            }
+        });
+        loan_rate_edit.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (!s.toString().equals(loan_rate_result[0])) {
+//                    Log.i(TAG, " a : " + s.toString());
+                    if (s.length() == 0) {
+                        loan_rate_edit.setText(0 + "");
+                        return;
+                    }
+
+                    Float loanPrice = Float.parseFloat(loan_price_edit.getText().toString().replaceAll(",", ""));
+                    Float loanRate = Float.parseFloat(loan_rate_edit.getText().toString().replaceAll(",", ""));
+                    Float yearlyRevenuCalResult = (loanPrice * loanRate) / 100;
+
+                    yearly_interest_edit.setText(df.format(Math.round(yearlyRevenuCalResult)));
+                }
+            }
+        });
+
+        // 3st Layout
+        yearly_rent_revenu_edit.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    Long yearlyRentRevenu = Long.parseLong(yearly_rent_revenu_edit.getText().toString().replaceAll(",", ""));
+                    Long yearlyInterest = Long.parseLong(yearly_interest_edit.getText().toString().replaceAll(",", ""));
+                    Long yearlyPureRevenuCalResult = yearlyRentRevenu - yearlyInterest;
+
+                    yearly_pure_revenu_edit.setText(df.format(yearlyPureRevenuCalResult));
+            }
+        });
+        yearly_interest_edit.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                Long yearlyRentRevenu = Long.parseLong(yearly_rent_revenu_edit.getText().toString().replaceAll(",", ""));
+                Long yearlyInterest = Long.parseLong(yearly_interest_edit.getText().toString().replaceAll(",", ""));
+                Long yearlyPureRevenuCalResult = yearlyRentRevenu - yearlyInterest;
+
+                yearly_pure_revenu_edit.setText(df.format(yearlyPureRevenuCalResult));
+            }
+        });
+        yearly_pure_revenu_edit.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                Float yearlyPureRevenu = Float.parseFloat(yearly_pure_revenu_edit.getText().toString().replaceAll(",", ""));
+                Long monthlyPureRevenuCalResult = (long)Math.round(yearlyPureRevenu)/12;
+
+                monthly_pure_revenu_edit.setText(df.format(monthlyPureRevenuCalResult)); // 월 순수익
+
+                Float realInvestment = Float.parseFloat(real_investment_edit.getText().toString().replaceAll(",", ""));
+                Float rentRevenuRate = (yearlyPureRevenu / realInvestment) * 100;
+                Log.i(TAG, "realInvenstMent : "+ realInvestment);
+                Log.i(TAG, "yearlyPureRevenu : " + yearlyPureRevenu);
+                Log.i(TAG, "rentRevenuRate : " + rentRevenuRate);
+                rent_rate_edit.setText(rentRevenuRate + "");
+            }
+        });
+        real_investment_edit.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                Float yearlyPureRevenu = Float.parseFloat(yearly_pure_revenu_edit.getText().toString().replaceAll(",", ""));
+                Float realInvestment = Float.parseFloat(real_investment_edit.getText().toString().replaceAll(",", ""));
+                Float rentRevenuRate = (yearlyPureRevenu / realInvestment) * 100;
+
+                rent_rate_edit.setText(rentRevenuRate + "");
+            }
+        });
 
     }
 
